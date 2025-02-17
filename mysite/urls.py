9e16bb01dc682e679997209c9b72461977cafd05
Mehdi <Mahdi.tb95@gmail.com>
2025-02-17 22:50:32 +0330

    A new link for welcome page is added

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include  # new
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),  # new
+    path("", include("welcome.urls")),
 ]
