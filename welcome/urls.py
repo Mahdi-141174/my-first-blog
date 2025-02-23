fa4ac76eece73c80f927e38cff4910d2a056954f
Mehdi <Mahdi.tb95@gmail.com>
2025-02-23 20:52:52 +0330

    some new templates are made for home page, sign-up and -in

--- welcome/urls.py
+++ welcome/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("welcome/", views.welcome, name="welcome"),
+    path("", views.home, name="home"),
+    path("sign_up/", views.sign_up, name="sign_up"),
+    path("sign_in/", views.sign_in, name="sign_in"),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -5,4 +5,16 @@ def welcome(request):
     return render(request, "welcome/welcome.html", {})
 
 
+def home(request):
+    return render(request, "welcome/home.html", {})
+
+
+def sign_up(request):
+    return render(request, "welcome/sign_up.html", {})
+
+
+def sign_in(request):
+    return render(request, "welcome/sign_in.html", {})
+
+
 # Create your views here.
